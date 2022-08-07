b9d5744afdf665777e7eec77635c9924934532f9
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-08 00:15:24 +0900

    selector 추가, questionsLenthState

--- src/components/ActionButtons/ActionButtons.js
+++ src/components/ActionButtons/ActionButtons.js
@@ -2,13 +2,12 @@ import Button from 'components/Button';
 import useStep from 'hooks/useStep';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import questionsState from 'stores/questions/atom';
+import questionsLengthState from 'stores/questions/questionsLengthState';
 import styled from 'styled-components';
 
 function ActionButtons() {
   const step = useStep();
-  const questions = useRecoilValue(questionsState);
-  const questionsLength = questions.length;
+  const questionsLength = useRecoilValue(questionsLengthState);
 
   const isLast = questionsLength - 1 === step;
   const navigate = useNavigate();
